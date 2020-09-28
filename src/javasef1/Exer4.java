9e64885cd5babf7efc8c446b428af4dfc4ce5d49
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 16:31:15 -0400

    Variables.

---
+++ src/javasef1/Exer4.java
@@ -0,0 +1,14 @@
+package javasef1;
+
+public class Exer4 {
+	public static void main(String[] args) {
+		//coment
+		String name = "Mateus".toUpperCase();
+		String second = "Borja".toUpperCase();
+		String space = " ";
+		
+		System.out.println(name +  space + second);
+		
+	}
+
+}
