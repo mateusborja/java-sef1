52f6e8236a599e1f8cccde5b4f8dc82f96f5a99a
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 16:37:24 -0400

    Variables and usable

---
+++ src/javasef1/Exer5.java
@@ -0,0 +1,44 @@
+package javasef1;
+
+public class Exer5 {
+	public static void main(String[] args) {
+		boolean var1;
+		var1  = true;
+		
+		boolean var2;
+		var2  = false;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		byte var3 = 10;
+		System.out.println(var3);
+		
+		var3 = 100;
+		System.out.println(var3);
+		
+		short var4 = 200;
+		System.out.println(var4);
+		
+		int var5 = 10000;
+		System.out.println(var5);
+		
+		float var6 = 14.14f;
+		System.out.println(var6);
+		
+		double var7 = 100.50;
+		System.out.println(var7);
+		
+		char var8 = 'a';
+		System.out.println(var8);
+		
+		char var9 = '\u00aE';
+		System.out.println(var9);
+		
+		char var10 = 112;
+		System.out.println(var10);
+		
+		
+	}
+
+}
