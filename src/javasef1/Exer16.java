1d7adf8040164f5603de60304ef5922c5810c863
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 19:15:56 -0400

    java if and else

---
+++ src/javasef1/Exer16.java
@@ -0,0 +1,29 @@
+package javasef1;
+
+import javax.swing.JOptionPane;
+
+public class Exer16 {
+	public static void main(String[] args) {
+		int var1 = 10;
+		int var2 = 20;
+		String solution1 = "Solução Verdadeira";
+		String solution2 = "Solução Falsa";
+		
+		if (var1 > var2) {
+			System.out.println(solution1); 
+		} else {
+			System.out.println(solution2); 
+		}
+		
+		int var3 = 10;
+		if (var3 > 5) {
+			System.out.println(solution1);
+		} else {
+			System.out.println(solution2);
+
+		}
+	}
+	
+
+
+}
