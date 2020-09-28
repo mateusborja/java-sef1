66c223660b50765026089126dd7cfc5096649087
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 16:42:29 -0400

    variables increment

---
+++ src/javasef1/Exer6.java
@@ -0,0 +1,23 @@
+package javasef1;
+
+public class Exer6 {
+	public static void main(String[] args) {
+		byte meuByte = 10;
+		System.out.println(meuByte);
+		
+		short meuShort = meuByte;
+		System.out.println(meuShort);
+		
+		int meuInt = meuShort;
+		System.out.println(meuInt);
+		
+		float meuFloat = meuInt;
+		System.out.println(meuFloat);
+		
+		double meuDouble = meuFloat;
+		System.out.println(meuDouble);
+		
+				
+	}
+
+}
