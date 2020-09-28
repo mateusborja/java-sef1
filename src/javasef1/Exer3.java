1e0a696e6217b7c32094c27adfa8747d6d2b8353
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 16:24:21 -0400

    JOptionPane class and methods

---
+++ src/javasef1/Exer3.java
@@ -0,0 +1,11 @@
+package javasef1;
+
+import javax.swing.JOptionPane;
+
+public class Exer3 {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello World");
+		System.exit(0);
+	}
+
+}
