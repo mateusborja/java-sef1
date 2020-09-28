939a66fc017a454d4048ed067834b995cf645ca2
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 16:48:23 -0400

    Strings

---
+++ src/javasef1/Exer7.java
@@ -0,0 +1,19 @@
+package javasef1;
+
+public class Exer7 {
+	public static void main(String[] args) {
+		String primeironome = "Fabio";
+		System.out.println(primeironome);
+		String nome = "Joao".toUpperCase();
+		System.out.println(nome);
+		
+		String sobrenome = " da Siva".toUpperCase();
+		System.out.println(nome + sobrenome);
+		
+		primeironome = "fer" + nome;
+		
+		
+		
+	}
+
+}
