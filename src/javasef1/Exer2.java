65844bd107ed87869a9d685b174da0b1e4ed33c5
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 16:21:41 -0400

    system.out.println();

---
+++ src/javasef1/Exer2.java
@@ -0,0 +1,11 @@
+package javasef1;
+
+public class Exer2 {
+	public static void main(String[] args) {
+		System.out.println("Mateus Borja");
+		System.out.println("42 anos");
+		System.out.println("Java Programmer");
+		System.out.println("Rua Tucanos, N. 09");
+	}
+
+}
