972fdf1624350b46f0df5f0cd15d258819a10257
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 18:46:33 -0400

    operators

---
+++ src/javasef1/Exer12.java
@@ -0,0 +1,19 @@
+package javasef1;
+
+public class Exer12 {
+	
+	public static void main(String[] args) {
+		int dez = 10;
+		int vinte = 20;
+		
+		boolean res = dez > vinte;
+		System.out.println(res);
+		
+		res = dez ==vinte;
+		System.out.println(res);
+		
+		
+		
+		
+	}
+}
