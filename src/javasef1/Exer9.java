cd52a6e01285e1942cd5aad00941052ce0ee7c5d
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 16:56:58 -0400

    Arithmetics operators

---
+++ src/javasef1/Exer9.java
@@ -0,0 +1,17 @@
+package javasef1;
+
+public class Exer9 {
+
+	public static void main(String[] args) {
+		String nome = "Mateus";
+		String espaco = " ";
+		String sobrenome = "Borja";
+		String completo = nome + espaco + sobrenome;
+		System.out.println(completo);
+		
+		String silva = completo + " da silva";
+		System.out.println(silva);
+		
+		
+	}
+}
