035a8edfeef1ab898310d334ca5452a2c9877a5e
Mateus Borja <mateus.borja@gmail.com>
2020-09-28 16:52:52 -0400

    operators

---
+++ src/javasef1/Exer8.java
@@ -0,0 +1,25 @@
+package javasef1;
+
+public class Exer8 {
+	public static void main(String[] args) {
+		int var1 = 1 + 1;
+		System.out.println(var1);
+		
+		int var2 = 10 - var1;
+		System.out.println(var2);
+		
+		int var3 = var2 * 2;
+		System.out.println(var3);
+		
+		int var4 = var2 / 2;
+		System.out.println(var4);
+		
+		var4 = 7;
+		int var5 = var4 % 2;
+		System.out.println(var5);
+		
+		
+	
+}
+
+}
