99f8c6145109c22579bb1045fe7aedf0bf3e45e2
Mateus Borja <mateus.borja@gmail.com>
2020-09-29 10:30:19 -0400

    while control

---
+++ src/javasef1/Exer19.java
@@ -0,0 +1,24 @@
+package javasef1;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Exer19 {
+	public static void main(String[] args) {
+		int var1 = 1;
+		System.out.println(var1);
+		
+		while (var1 <= 8) {
+			System.out.println("Executando");
+			var1++;
+			
+		}
+		
+		System.out.println("finalizado");
+		
+		
+	}		
+
+}
+
+
