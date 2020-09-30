c61f54be7c8ab4b2270de89b7cd587ed31af742a
Mateus Borja <mateus.borja@gmail.com>
2020-09-30 00:00:07 -0400

    practice3 class

--- src/practice/practice3.java
+++ src/practice/practice3.java
@@ -3,11 +3,7 @@ package practice;
 public class practice3 {
 	public static void main(String[] args) {
 		
-		String texto = """
-				Esse é um texto baseado numa String
-			""";
-		System.out.println(texto);
-		
+	
 		
 	}
 
