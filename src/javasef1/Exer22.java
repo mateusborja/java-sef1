ad71fac7edfec6cf966f61ea822dc318e57697d7
Mateus Borja <mateus.borja@gmail.com>
2020-10-03 19:24:30 -0400

    java do while

---
+++ src/javasef1/Exer22.java
@@ -0,0 +1,17 @@
+package javasef1;
+
+//java do while
+public class Exer22 {
+	public static void main(String[] args) {
+		int v1 = 1;
+		System.out.println("programa..." + " \n");
+		do {
+			System.out.println("-> executando" + " " + v1 + "\n");
+			v1++;
+			
+		} while (v1 <=5);
+		System.out.println("finalizando...");
+			
+	}
+
+}
---
+++ src/practice/practice12.java
@@ -0,0 +1,16 @@
+package practice;
+
+//java do while
+public class practice12 {
+	public static void main(String[] args) {
+		int var1 = 1;
+		
+		do {
+			System.out.println("executando o bloco 1 ->" + " " + var1 + "\n");
+			var1++;
+			
+		} while (var1 <=10);
+		System.out.println("-> finalizado... " + "\n" + "-> executando o bloco 2");
+	}
+
+}
