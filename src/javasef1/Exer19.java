3892572b0b68e6ed6bff0ca00438ec541e5bb759
Mateus Borja <mateus.borja@gmail.com>
2020-10-03 15:47:54 -0400

    java while

--- src/javasef1/Exer19.java
+++ src/javasef1/Exer19.java
@@ -1,20 +1,17 @@
 package javasef1;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
+//java while
 public class Exer19 {
 	public static void main(String[] args) {
-		int var1 = 1;
-		System.out.println(var1);
+		int var1 = 0;
 		
-		while (var1 <= 8) {
-			System.out.println("Executando");
+		while (var1 < 8) {
+			System.out.println(var1);
 			var1++;
 			
 		}
 		
-		System.out.println("finalizado");
+		System.out.println("\n" + "finalizado...");
 		
 	}		
 
---
+++ src/practice/practice6.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.util.Scanner;
+
+//Abreviation Name app
+public class practice6 {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Digite o nome completo: ");
+		String nomeCompleto = input.nextLine();
+
+		String nomePedacos[] = nomeCompleto.split(" ");
+		
+		for (String letra : nomePedacos){
+
+			System.out.print(letra.substring(0, 1) + ".");	
+		}
+
+	}
+	
+}
