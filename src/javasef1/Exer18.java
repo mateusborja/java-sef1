122aa4b6ba8b9f857d334c2493dd9c6daf61bfe6
Mateus Borja <mateus.borja@gmail.com>
2020-10-03 15:33:51 -0400

    java if else numbers exercice

--- src/javasef1/Exer18.java
+++ src/javasef1/Exer18.java
@@ -9,20 +9,22 @@ public class Exer18 {
 		String v1 = JOptionPane.showInputDialog("Digite o numero 1?");
 		String v2 = JOptionPane.showInputDialog("Digite o numero 2?");
 		
+		
 		//conversao de string para primitivos
-		int vlr1 = Integer.parseInt(v1);
-		int vlr2 = Integer.parseInt(v2);
+		int vl1 = Integer.parseInt(v1);
+		int vl2 = Integer.parseInt(v2);
 		
-		String relatorio = " ";
+		//logic
+		String relatorio = "";
 		
-		if (vlr1 > vlr2) {
+		if (vl1 > vl2) {
 			relatorio += v1; 
 		} else {
 			
-			if (vlr2 > vlr1) {
-				relatorio += vlr2;
+			if (vl2 > vl1) {
+				relatorio += vl2;
 			} else {
-				relatorio = "Iguais" + vlr1;
+				relatorio = "Iguais" + vl1;
 
 			}
 
