8d81eaabd713b8b182da6229cf5bd9fc1cf24fcc
Mateus Borja <mateus.borja@gmail.com>
2020-10-03 15:28:18 -0400

    birthday app with java if else

---
+++ src/practice/practice5.java
@@ -0,0 +1,28 @@
+package practice;
+
+import javax.swing.JOptionPane;
+
+//birthday app
+public class practice5 {
+	public static void main(String[] args) {
+		
+		String nome = JOptionPane.showInputDialog("Digite seu Nome?");
+		String idade = JOptionPane.showInputDialog(nome + " " + "qual sua Idade?");
+		String relatorio = "";
+		
+		int age = Integer.parseInt(idade);
+		
+		
+		if (age <=18) {
+			relatorio = nome + ", " + "você não pode tirar sua Habilitação.";
+		} else {
+			relatorio = nome + ", " + "você já pode tirar sua Habilitação.";
+		}
+		
+		JOptionPane.showMessageDialog(null, relatorio);
+		
+		System.exit(0);
+		
+	}
+
+}
