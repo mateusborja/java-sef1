3d92bc990a348fa0b1f4321a0dad9df0e70ca674
Mateus Borja <mateus.borja@gmail.com>
2020-10-03 19:59:26 -0400

    java for

---
+++ src/javasef1/Exer23.java
@@ -0,0 +1,15 @@
+package javasef1;
+//java for
+public class Exer23 {
+	public static void main(String[] args) {
+		System.out.println("-> Sistema executando..." + "\n");
+		
+		int ct;
+		for(ct=0; ct < 5; ct++) {
+			System.out.println("-> executando" + " " + ct);
+		}
+		
+		System.out.println("\n" + "-> saindo...");
+	}
+	
+}
---
+++ src/practice/practice13.java
@@ -0,0 +1,15 @@
+package practice;
+//java for examples
+public class practice13 {
+	public static void main(String[] args) {
+		System.out.println("-> executando sitema..." + "\n");
+		
+		int ct;
+		for (ct = 0; ct <=10; ct++) {
+			System.out.println("-> bloco for -> executando..." + " " +ct);
+		}
+		
+		System.out.println("\n" + "-> fim da execução...");
+	} 
+
+}
