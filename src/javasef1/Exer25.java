4dd0a8db6216615cae4ee62471286822de8b90cc
Mateus Borja <mateus.borja@gmail.com>
2020-10-03 20:35:05 -0400

    java break loop control

---
+++ src/javasef1/Exer25.java
@@ -0,0 +1,19 @@
+//java break
+package javasef1;
+
+public class Exer25 {
+	public static void main(String[] args) {
+		int i;
+		for (i=0; i<=10; i++) {
+			//break
+			if (i == 5) {
+				break;
+			}
+			System.out.println("System operation ->" + i);
+			
+		}
+		
+		System.out.println("\n" + "System Operation Over");
+	}
+
+}
