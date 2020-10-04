1960e101865f51dc8fede0f674855d2a7b7fa239
Mateus Borja <mateus.borja@gmail.com>
2020-10-04 16:39:34 -0400

    java switch no value

---
+++ src/javasef1/Exer29.java
@@ -0,0 +1,42 @@
+//java switch no value exist
+package javasef1;
+
+public class Exer29 {
+	public static void main(String[] args) {
+		int valor = 50;
+
+		switch (valor) {
+
+		case 10: {
+			System.out.println("o valor é 10");
+			System.out.println("-> case 1" + "\n");
+			//use break
+			break;
+		}
+
+		case 20: {
+			System.out.println("o valor é 20");
+			System.out.println("-> case 2" + "\n");
+			//use break
+			break;
+		}
+
+		case 30: {
+			System.out.println("o valor é 30");
+			System.out.println("-> case 3" + "\n");
+			//use break
+			break;
+		}
+
+		default: {
+			System.out.println("não é nenhum valor" + "\n");
+		}
+
+		}
+
+		System.out.println("o programa continua..." + "\n");
+	}
+
+}
+
+
