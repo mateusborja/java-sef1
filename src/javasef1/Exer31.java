f1226f9a2e176ea19c91c00837d655fcb90e02a4
Mateus Borja <mateus.borja@gmail.com>
2020-10-04 16:52:26 -0400

    java new scope

---
+++ src/javasef1/Exer31.java
@@ -0,0 +1,15 @@
+//java scope
+package javasef1;
+
+public class Exer31 {
+	public static void main(String[] args) {
+		//open scope with {}
+		{
+			int v1 = 19;
+			System.out.println(v1);
+		}
+		
+		
+	}
+
+}
