494648ddf25e40d686e9246dd6bb2625182ac303
Mateus Borja <mateus.borja@gmail.com>
2020-10-04 19:56:00 -0400

    java final variable

---
+++ src/javasef1/Exer38.java
@@ -0,0 +1,26 @@
+//final variable
+package javasef1;
+
+public class Exer38 {
+	public static void main(String[] args) {
+		
+		int i = 10;
+		System.out.println(i + "\n");
+		
+		i = 10;
+		System.out.println(i + "\n");
+		
+		final int e = 20;
+		System.out.println(e + "\n");
+		
+		
+		
+		
+		
+		
+		final int var1 = 10;
+		System.out.println(var1);
+		
+	}
+
+}
