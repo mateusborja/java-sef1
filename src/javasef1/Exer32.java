f9d0c917bde4f6efeac89f0dbd17c3172fc6d23b
Mateus Borja <mateus.borja@gmail.com>
2020-10-04 17:20:59 -0400

    java array

---
+++ src/javasef1/Exer32.java
@@ -0,0 +1,27 @@
+//java array
+package javasef1;
+
+public class Exer32 {
+	public static void main(String[] args) {
+		
+		int i = 10;
+		System.out.println(i + "\n");
+		
+		int[] colecao = new int[10];
+		colecao[0] = 1;
+		colecao[1] = 2;
+		colecao[2] = 3;
+		colecao[3] = 4;
+		colecao[4] = 5;
+		colecao[5] = 6;
+		colecao[6] = 7;
+		colecao[7] = 8;
+		colecao[8] = 9;
+		colecao[9] = 10;
+		
+		System.out.println(colecao[1] + colecao[2] + "\n");
+		System.out.println(colecao[3] + colecao[4]);
+		
+	}
+
+}
