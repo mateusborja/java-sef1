95a2b07d82ae9d4873ea69b8f46c1fa2966ffaf9
Mateus Borja <mateus.borja@gmail.com>
2020-10-04 16:28:48 -0400

    java switch

---
+++ src/javasef1/Exer27.java
@@ -0,0 +1,34 @@
+//java switch
+package javasef1;
+
+public class Exer27 {
+	public static void main(String[] args) {
+		int i = 20;
+
+		switch (i) {
+
+		case 10: {
+			System.out.println("é 10");
+			System.out.println("primeiro case" + "\n");
+		}
+
+		case 20: {
+			System.out.println("é 20");
+			System.out.println("segundo case" + "\n");
+		}
+
+		case 30: {
+			System.out.println("é 30");
+			System.out.println("terceiro case" + "\n");
+		}
+
+		default: {
+			System.out.println("default" + " -> não é nenhum valor esperado" + "\n");
+
+		}
+
+			System.out.println("-> o programa continua" + "\n");
+		}
+	}
+
+}
