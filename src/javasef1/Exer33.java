e1626f8926ebb6c53df08f44b82f6ad306530043
Mateus Borja <mateus.borja@gmail.com>
2020-10-04 17:26:51 -0400

    java array size

---
+++ src/javasef1/Exer33.java
@@ -0,0 +1,25 @@
+//java array size
+package javasef1;
+
+public class Exer33 {
+	public static void main(String[] args) {
+
+		String[] collection = new String[2];
+
+		collection[0] = "feijao";
+		collection[1] = "arroz";
+
+		System.out.println(collection[0] + "\n" + collection[1] + "\n");
+
+		long[] collection2 = new long[2];
+
+		collection2[0] = 1;
+		collection2[1] = 2;
+
+		System.out.println(collection2[0] + "\n" + collection2[1]);
+
+		// not exist
+		// collection[2] = 10;
+	}
+
+}
