935d0b0d365572a7afff83f7552e7e063f7c243f
mateus.borja <mateus.borja@10.1.9.46>
2020-10-05 13:50:56 -0400

    change compiler to jdk15

---
+++ src/practice/practice15.java
@@ -0,0 +1,16 @@
+//sum number 888
+package practice;
+
+public class practice15 {
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 788;
+		int sum = a + b;
+		int ct = 888;
+		int i;
+		
+
+
+	}
+
+}
