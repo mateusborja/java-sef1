a977a671e3b8dd523a8116aafa24f91c22f573cf
mateus.borja <mateus.borja@nbkdes021.institutoitn.com.br>
2020-10-06 17:08:04 -0400

    age app

---
+++ src/practice/practice16.java
@@ -0,0 +1,25 @@
+//age app
+package practice;
+
+import javax.swing.JOptionPane;
+
+public class practice16 {
+	public static void main(String[] args) {
+		String seunome = JOptionPane.showInputDialog("Qual seu Nome?");
+		String qtd = JOptionPane.showInputDialog(seunome + " quantos filhos você tem?");
+		int filhos = Integer.parseInt(qtd);
+		int ct = 1;
+		String rel = "";
+
+		while (ct <= filhos) {
+			String nomefilhos = JOptionPane.showInputDialog(seunome + "\n" + "Qual nome do seu " + ct + " filho?");
+			String idadefilhos = JOptionPane.showInputDialog(seunome + "\n" + "Qual a idade do seu " + ct + " filho?");
+			rel += "o nome do seu " + ct + " filho é " + nomefilhos + ", e sua idade é " + idadefilhos + "." + "\n";
+			ct++;
+		}
+		
+		JOptionPane.showMessageDialog(null, "Relatório" + " \n" + rel);
+
+	}
+
+}
